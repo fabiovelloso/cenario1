21a30de52af54927ec8308b14dbc4ff127ddea59
Fabio Velloso <git-tower@fabiovelloso.com.br>
2017-01-06 02:01:19 -0200

    inserindo commit

--- init.sql
+++ init.sql
@@ -6,3 +6,4 @@ Insert into ORIGINAL.GERENTES (ID,NOME) values ('2','Chan');
 Insert into ORIGINAL.GERENTES (ID,NOME) values ('3','Clovis');
 Insert into ORIGINAL.GERENTES (ID,NOME) values ('4','Flavio');
 Insert into ORIGINAL.GERENTES (ID,NOME) values ('5','Kanashiro');
+commit;
